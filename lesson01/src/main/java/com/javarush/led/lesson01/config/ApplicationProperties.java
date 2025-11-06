2e7408f165e031cac6a2bf2c788cf71e7ec525e2
dr0n <pyatigin.a.a@gmail.com>
2025-11-07 00:07:27 +0500

    Lesson01 END

--- lesson01/src/main/java/com/javarush/led/lesson01/config/ApplicationProperties.java
+++ lesson01/src/main/java/com/javarush/led/lesson01/config/ApplicationProperties.java
@@ -5,6 +5,7 @@ import lombok.ToString;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.io.FileReader;
@@ -17,7 +18,7 @@ import java.util.Properties;
 
 @Slf4j
 @ToString
-
+@Component
 public class ApplicationProperties extends Properties {
 
 
@@ -27,10 +28,20 @@ public class ApplicationProperties extends Properties {
     public static final String HIBERNATE_CONNECTION_DRIVER_CLASS = "hibernate.connection.driver_class";
     public static final String ENV_EXPRESSION = "\\$\\{[A-Z_]+:.+}";
 
-    @SneakyThrows
-    public ApplicationProperties() {
-        this.load(new FileReader(CLASSES_ROOT + "/application.properties"));
+
+    public ApplicationProperties(
+            String url,
+            String username,
+            String password,
+            String driverClass
+    )
+    {
+        this.put(HIBERNATE_CONNECTION_URL, url);
+        this.put(HIBERNATE_CONNECTION_USERNAME, username);
+        this.put(HIBERNATE_CONNECTION_PASSWORD, password);
+        this.put(HIBERNATE_CONNECTION_DRIVER_CLASS, driverClass);
     }
+
     public final static Path CLASSES_ROOT = Paths.get(URI.create(
             Objects.requireNonNull(
                     ApplicationProperties.class.getResource("/")
